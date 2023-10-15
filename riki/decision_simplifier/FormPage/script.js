01e8e45618a34b2c55160c41910cb577ecc80678
RossGreidan <arturgreidan@gmail.com>
2023-10-15 16:12:26 +0300

    Visual for mainPage and for Forms

--- riki/decision_simplifier/FormPage/script.js
+++ riki/decision_simplifier/FormPage/script.js
@@ -1,20 +1,46 @@
-$(document).ready(function() 
-{
-    $(document).ready(function() 
-    {
-        $('#myForm').submit(function(e) 
-        {
-            e.preventDefault();
-            var textValue = $('#text').val();
-            var textareaValue = $('#textarea').val();
-            var selectValue = $('#select').val();
+$(document).ready(function() {
+    const step1 = $("#step1");
+    const step2 = $("#step2");
+    const step3 = $("#step3");
 
-            var outputText = "Текстовое поле: " + textValue + "<br>" +
-                            "Большое текстовое поле: " + textareaValue + "<br>" +
-                            "Выпадающий список: " + selectValue;
+    step1.show();
+    step2.hide();
+    step3.hide();
 
-            $('#output').html(outputText);
-            $('#myModal').modal('show');
-        });
+    $("#nextStep").click(function() {
+        step1.hide();
+        step2.show();
+    });
+
+    // Обработка отправки формы на первом этапе
+    $('#step1Form').submit(function(e) {
+        e.preventDefault();
+        step1.hide();
+        step2.show();
+    });
+
+    // Обработка отправки формы на втором этапе
+    $('#step2Form').submit(function(e) {
+        e.preventDefault();
+        step2.hide();
+        step3.show();
+    });
+
+    const variants = $("#variants");
+    let variantCount = 1;
+
+    $("#addVariant").click(function() {
+        variants.append(`
+            <div class="variant">
+                <label for="variant${variantCount}">Variants:</label>
+                <input type="text" name="variant${variantCount}" required>
+                <button class="removeVariant">Noņemt</button>
+            </div>
+        `);
+        variantCount++;
+    });
+
+    variants.on("click", ".removeVariant", function() {
+        $(this).parent().remove();
     });
 });
