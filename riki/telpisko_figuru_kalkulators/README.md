2a8f22339840468c9b4f8bf9045b3a1806eca72d
Markuss Adr <markuss.adrickis.skola@gmail.com>
2025-04-21 23:59:02 +0300

    Neliels atjauninājums
    Tagad ir vietās salikti komentāri.

--- riki/telpisko_figuru_kalkulators/README.md
+++ riki/telpisko_figuru_kalkulators/README.md
@@ -1,1 +1,20 @@
+####  ###       ###
+#   #   #          #
+####    #  ###  ###
+#       #          #
+#     ####      ###
 
+
+Sveicināti mūsu telpisko figūru kalkulātoru.
+Lietojot formulu lapas reizēm var apmulsināt, kur nu vēl rakstīt pareizus skaitļus kalkulātorā?
+
+!! "Kā lietot?" !!
+Pārlūkā atvērt "index.html"
+
+### Versija 0.1.1 ###
+-README.md nedaudz vairāk lasāms
+-Komentāri
+
+### Versija 0.1 ###
+-Iniciālais izlaidums
+-Konusa rēķins
--- riki/telpisko_figuru_kalkulators/script.js
+++ riki/telpisko_figuru_kalkulators/script.js
@@ -1,40 +1,43 @@
+//Vērtību iegūšana
 const number1 = document.getElementById("number1");
 const number2 = document.getElementById("number2");
 const result = document.getElementById("result");
 const form = document.getElementById("form");
 const calculationType = document.getElementById("calculationType");
 
+// Konusa funkcijas
 function volumeCone(R, H) {
     return (1 / 3) * Math.PI * R * R * H;
 }
-
 function surfaceCone(R, L) {
     return Math.PI * R * R + Math.PI * R * L;
 }
-
+// Nospiežot pogu ar id 'submit', notiks sekojošais:
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
+    // Piešķir mainīgajiem vērtības.
     let R = parseFloat(number1.value);
     let H_or_L = parseFloat(number2.value);
     let selectedCalc = calculationType.value;
-
+    // Izvadvērtība
+    let output;
+    // Ja Radiusa lauks vai 2. lauks ir tukš
     if (!R || !H_or_L) {
         alert('Abiem laukiem jābūt aizpildītiem ar skaitļiem!');
         return;
     }
-
+    // Nevar būt negatīvas vērtības ģeometriskām
     if (R <= 0 || H_or_L <= 0) {
         alert('Abiem skaitļiem jābūt lielākiem par nulli!');
         return;
-    }
-
-    let output;
-
+    } 
+    // Tilpuma funkcijas izsaukums
     if (selectedCalc === "volume") {
         output = volumeCone(R, H_or_L);
         result.textContent = `Tilpums V = ${output.toFixed(2)}`;
-    } else if (selectedCalc === "surface") {
+    } 
+    // Ja tomēr ir virsmas laukums 
+    else if (selectedCalc === "surface") {
         output = surfaceCone(R, H_or_L);
         result.textContent = `Virsmas laukums S = ${output.toFixed(2)}`;
     }
