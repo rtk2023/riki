9e52028187bb59a2fd24f661c77ff2dc6957eec7
Laila <laila.bicala@gmail.com>
2025-05-02 15:53:41 +0300

    JS code small update

--- riki/saules_panelu_kalkulators/script.js
+++ riki/saules_panelu_kalkulators/script.js
@@ -1,1 +1,13 @@
 // Saulesp paneļu aprēķinu veiksānai
+
+function aprekinatButton() { // Funkcija, kas pārbauda datus pirms aprēķina veikšanas
+    //
+}
+
+function laikapstakluDati() { // funkcija, kas iegūst datus no laikapstaklu API
+    //
+}
+
+function aprekins() { // funkcija, kas veic saules paneļu ietaupijuma aprekinus
+    //
+} 
