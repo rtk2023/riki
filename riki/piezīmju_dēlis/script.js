613e7663c5ed71e6b3840ff8649cee83f48e67b0
PhoenixDev <147312817+RealPhoenixDev@users.noreply.github.com>
2025-05-08 23:53:38 +0300

    removed junk

--- riki/piezīmju_dēlis/script.js
+++ riki/piezīmju_dēlis/script.js
@@ -1,48 +1,14 @@
 const openTaskBoardBtn = document.getElementById("openTaskBoardBtn");
 const openSettingsBtn = document.getElementById("openSettingsBtn");
 
-function loadUsernameData() {
-  const username_span = document.getElementById("username_span");
-  const container = document.getElementById("usernameWindowContainer");
-
-  const username_input = document.getElementById("username_input");
-  const username_submit = document.getElementById("username_submit");
-
-  let popup_open = false;
-
-  if (!localStorage.getItem("username")) {
-    container.style.display = "flex";
-    windowHistory.push("usernameWindow");
-    username_input.addEventListener("keypress", function (event) {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        username_submit.click();
-      }
-    });
-    username_submit.addEventListener("click", (e) => {
-      const username = username_input.value;
-      localStorage.setItem("username", username);
-      container.style.display = "none";
-      windowHistory.pop();
-
-      // username_span.innerHTML = localStorage.getItem("username");
-    });
-  }
-  // username_span.innerHTML = localStorage.getItem("username");
-}
-
-document.addEventListener("loaded", () => {
-  //   loadUsernameData();
-});
-
 openTaskBoardBtn.addEventListener("click", () => {
-  if (windowHistory.length !== 0) return;
-  taskBoardContainer.style.display = "flex";
-  windowHistory.push("taskBoard");
+	if (windowHistory.length !== 0) return;
+	taskBoardContainer.style.display = "flex";
+	windowHistory.push("taskBoard");
 });
 
 openSettingsBtn.addEventListener("click", () => {
-  if (windowHistory.length !== 0) return;
-  optionContainer.style.display = "flex";
-  windowHistory.push("optionContainer");
+	if (windowHistory.length !== 0) return;
+	optionContainer.style.display = "flex";
+	windowHistory.push("optionContainer");
 });
