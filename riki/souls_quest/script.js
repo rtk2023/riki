0a85603ce89841ba1224f430092172d789e8ca6f
RainersN <r.ninuks@gmail.com>
2026-04-15 22:30:18 +0300

    Improve filter backend and frontend
    Improved the filter option by letting users select multiple filters at once,aswell as darkening selected filter buttons to show what filters are being used.

--- riki/souls_quest/script.js
+++ riki/souls_quest/script.js
@@ -1,3 +1,6 @@
+let filterList = [];
+let btnContainer = document.getElementById("myBtnContainer");
+let btns = btnContainer.getElementsByClassName("btn");
 function changeModalContent(id){
 	let modalContent = [
 	{title : "Human with beard and skirt",content : "Dark Souls[a] is a dark fantasy action role-playing game series developed by FromSoftware and published by Bandai Namco Entertainment. Created by Hidetaka Miyazaki, the series began with the release of Dark Souls (2011) and has seen two sequels, Dark Souls II (2014) and Dark Souls III (2016)."},
@@ -11,18 +14,39 @@ function changeModalContent(id){
 	document.getElementById("exampleModalContent").innerHTML = (modalContent[id].content);
 	new bootstrap.Modal(document.getElementById('exampleModal')).show();
 }
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
 
+function filterSelection(c) {
+	let x = document.getElementsByClassName("filterDiv");
+		for (let i = 0; i < x.length; i++) {
+			w3RemoveClass(x[i], "show");
+			if (x[i].className.indexOf("") > -1) w3AddClass(x[i], "show");
+		}
+	if(filterList.indexOf(c) != -1){
+		let itemIndex = filterList.indexOf(c);
+		filterList = filterList.filter(function(itemIndex) {
+		return itemIndex !== c
+		})
+	}
+	else{
+		filterList.push(c);
+	}
+	if (c == "all") {
+		c = "";
+		filterList = [];
+	}
+	console.log(filterList);
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+	for (let i = 0; i < filterList.length;i++){
+		for (let j = 0; j < x.length; j++) {
+			if(x[j].classList.contains("show")== true){
+				console.log(filterList[i]);
+				w3RemoveClass(x[j], "show");
+				if (x[j].className.indexOf(filterList[i]) > -1) w3AddClass(x[j], "show");
+			}
+		}
+	}
+}
+filterSelection("all");
 // Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
@@ -49,12 +73,22 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+	for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+		if(this.classList.contains("showAll")){
+			let current = btnContainer.getElementsByClassName("btn");
+			for(let j = 0;j<current.length;j++){
+				w3RemoveClass(current[j],"active");
+			}
+			if(!this.classList.contains("active")){
+			this.className += " active";
+			}
+		}
+		else if(this.classList.contains("active")){
+			w3RemoveClass(this,"active");
+		}
+		else if(!this.classList.contains("active")){
+			this.className += " active";
+		}
+    });
+  }
