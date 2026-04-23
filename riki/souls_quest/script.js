42637ba008f2d071fb00fbe287ad61534de9f211
RainersN <r.ninuks@gmail.com>
2026-04-23 12:40:43 +0300

    Add filters and content

--- riki/souls_quest/script.js
+++ riki/souls_quest/script.js
@@ -11,18 +11,18 @@ const modalContent = [
 	[{title : "Siegmeyer of Catarina",content : "A man with wierd, onion shaped armor, behaving very confused every time you meet him."},{title : "Siegmeyers' location",content : "Found next to Sen's Fortress gate "}],
 	[{title : "Laurentius of the Great Swamp",content : "A very emotional and human man, a scholar of pyromancy, that can teach the player to use it."},{title : "Laurentius' location",content : "Upon going into depths, drop down next to the butcher enemy, after entering a room full of barrels, he will call out to you to free him."}],
 	[{title : "Ingward",content : "A mysterious sorcerer that covers his face with a mask. He can reverse a curse that the player can obtain either from Obelisk enemies un the Depths or Seath the Scaleless at Duke's Archives."},{title : "Ingwards' location",content : "Deep inside of New Londo Ruins beneath Firelink Shrine."}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}],
-	[{title : "",content : ""},{title : "",content : ""}]
+	[{title : "Griggs of Vinheim",content : "A sorcerer found imprisoned, upon freeing him, he continues on his quest following his master, Big Hat Logan, in return for saving him, he will teach the player various sorceries."},{title : "Griggs' location",content : "Lower Undead Burg, behind a closed door. (key can be obtained from the Undead Merchant or you can use starting gift Master Key.)"}],
+	[{title : "Big Hat Logan",content : "A master sorcerer with a big hat covering his face. Pursues knowledge and wisdom."},{title : "Logans' location",content : "Sen's Fortress, behind a wall that a snake rests upon, can be freed by either using a key of Sen's Fortress or the Master Key."}],
+	[{title : "Knight Lautrec of Carim",content : "A very wise-cracking man that helps the player at first."},{title : "Lautrecs' location",content : "Locked in a cell within Bell Gargoyle's tower."}],
+	[{title : "Patches the Hyena",content : "Bald, slippery bastard that stabs you in the back, if you have the heart to forgive him, he will return to the Firelink Shrine and become a merchant."},{title : "Patches' location",content : "Found at the Tomb of The Giants beneath the Catacombs that can be accessed through the graveyard of Firelink Shrine."}],
+	[{title : "Elizabeth",content : "A mushroom lady."},{title : "Elizabeths' location",content : "Found in the Artorias DLC, the first NPC you meet."}],
+	[{title : "Reah of Thorolund",content : "A maiden from House of Thorolund. Is on a misson to find the Rite of Kindling."},{title : "Reah's location",content : "After defeating the Capra Demon in Lower Undead Burg will appear next to Petrus and her two guards."}],
+	[{title : "Dusk of Oolacile",content : "A mysterious woman trapped inside a Crystal Golem next to the Hydra. A neccesary NPC to access the Artorias' dlc."},{title : "Dusks' location",content : "Darkroot Basin, spawns next to defeated Hydra, upon giving her a talisman obtained from Duke's Archives, you can access the DLC."}],
+	[{title : "Kingseeker Frampt",content : "A primordial serpent serving the Lord Gwyn."},{title : "Frampts' location",content : "Appears in the Firelink Shrine after ringing the both bells of awakening."}],
+	[{title : "Daughter of Izalith",content : "Sister of Quelaag. Half spider half human hybrid."},{title : "Quelaags' sisters location",content : "Found behind an illusory wall after defeating Quelaag."}],
+	[{title : "Alvina of the Darkwood Wood",content : "A feline that can give you the access to a covenant."},{title : "Alvinas' location",content : "Found in temple ruins behind the door locked with Crest of Artorias (you can buy it from Blacksmith Andre.) "}],
+	[{title : "Rickert of Vinheim",content : "A blacksmith locked behind bars for unknown reason."},{title : "Rickerts' location",content : "Found in New Londo Ruins right next to elevator, player needs to find the hidden path to him."}],
+	[{title : "Quelana of Izalith",content : "One of 7 daughters of Izalith."},{title : "Quelanas' location",content : "Found in the Great Swamps."}]
 	];
 document.getElementById("nextPageButton").addEventListener("click", function() {
 		pageNumber++;
